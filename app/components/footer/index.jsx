2646a77804013ada56646fa9f0cc0738a2b97e67
mycomputer <muhammadarslanjabbar@gmail.com>
2024-03-01 16:06:38 +0500

    contact-page

--- app/components/footer/index.jsx
+++ app/components/footer/index.jsx
@@ -7,12 +7,16 @@ function Footer({bg, logo, btnbg}) {
   return (
     <div>
         <div className={`w-full h-[259px] flex flex-col justify-center items-center bg-[${bg}] text-[#ffffff]`}>
-            <Image src={logo} alt='logo' width={75} height={64} className='mb-8'/>
+            {/* <Image src={logo} alt='logo' width={75} height={64} className='mb-8'/> */}
+            <div className={`flex flex-col text-[${btnbg}] text-[24px] leading-[20px] mb-5 font-[400]`}>
+              <div>Studio</div>
+              <div>Stash</div>
+            </div>
             <div onClick={() => router.push('/contact')} className={`h-[45px] w-[148px] rounded-[42px] text-[${bg}] pt-[10px] cursor-pointer bg-[${btnbg}]`}>
                 <center>Contact</center>
             </div>
         </div>
-        <div className='w-full flex justify-center items-center h-[49px] bg-[#ffffff] text-[16px] leading-[16px] font-[600]'>
+        <div className={`w-full flex justify-center items-center h-[49px] bg-[${btnbg}] text-[${bg}] text-[16px] leading-[16px] font-[600]`}>
             ©2024 StudioStash
         </div>
     </div>
